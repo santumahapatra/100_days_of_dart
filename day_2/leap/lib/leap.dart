d1e795922f9f01cbc4be7dbbe414640a84f10318
Santu Mahapatra <santumahapatra@gmail.com>
2020-05-06 17:28:00 +0530

    Leap year - shorter solution

--- day_2/leap/lib/leap.dart
+++ day_2/leap/lib/leap.dart
@@ -1,16 +1,19 @@
 class Leap {
-  bool leapYear(int year){
-    bool result = false;
-    if (year % 400 == 0){
-      result = true;
-    }
-    else if (year % 100 == 0){
-      result = false;
-    }
-    else if (year % 4 == 0) {
-      result = true;
-    }
+  // bool leapYear(int year){
+  //   bool result = false;
+  //   if (year % 400 == 0){
+  //     result = true;
+  //   }
+  //   else if (year % 100 == 0){
+  //     result = false;
+  //   }
+  //   else if (year % 4 == 0) {
+  //     result = true;
+  //   }
 
-    return result;
-  }
+  //   return result;
+  // }
+
+  bool leapYear(int year){
+    return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))? true: false;  }
 }
