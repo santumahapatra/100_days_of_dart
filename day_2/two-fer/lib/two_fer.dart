febe43638e586d3f0be4c10a1dd21ea252f3b274
Santu Mahapatra <santumahapatra@gmail.com>
2020-05-06 16:26:43 +0530

    Refactor Two Fer

--- day_2/two-fer/lib/two_fer.dart
+++ day_2/two-fer/lib/two_fer.dart
@@ -1,10 +1,3 @@
-String twoFer([String name]) {
-  // Put your code here
-  String message = "One for you, one for me.";
-  if (name == null){
-    return message;
-  }
-  else {
-    return message.substring(0, 8) + name + message.substring(11, );
-  }
+String twoFer([String name = 'you']) {
+  return 'One for $name, one for me.';
 }
