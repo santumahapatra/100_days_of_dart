4abca5bda90159add55ad9b71330819177cb93c8
Santu Mahapatra <santumahapatra@gmail.com>
2020-05-20 16:53:07 +0530

    Fixing the promises problem from yesterday

--- day_15/futures.dart
+++ day_15/futures.dart
@@ -1,15 +1,14 @@
 // Part 1
 //You can call the provided async function fetchRole to return the user role
 Future<String> reportUserRole() async {
-  // your implementation here
   String role = await fetchRole();
-  return Future.delayed(Duration(seconds: 3), () => 'User role: $role');
+  return 'User role: $role';
 }
 
 // Part 2
 // Implement reportLogins here
 // You can call the provided async function fetchLoginAmount to return the number of times that the user has logged in.
-Future<String> reportLogins async(){
+Future<String> reportLogins() async {
   int logins = await fetchLoginAmount();
-  return Future.delayed(Duration(seconds: 3), () => 'Total number of logins: $logins');
+  return 'Total number of logins: $logins';
 }
