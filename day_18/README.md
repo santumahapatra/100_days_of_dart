d458b2a747cab4c249bd38c30b4bf4685dabc32e
Santu Mahapatra <santumahapatra@gmail.com>
2020-05-22 23:52:02 +0530

    Sets

---
+++ day_18/README.md
@@ -0,0 +1,5 @@
+# Day 18
+
+# Problems Solved:
+
+* Sets
---
+++ day_18/sets.dart
@@ -0,0 +1,6 @@
+void main() {
+  Set<String> aSetOfStrings = {'one', 'two', 'three'};
+  aSetOfStrings.add('two');
+  print(aSetOfStrings);
+  print(aSetOfStrings.contains('one'));
+}
