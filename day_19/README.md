07d0424c8e1d13ec4a12f2e2ac15f087c14fbaef
Santu Mahapatra <santumahapatra@gmail.com>
2020-05-23 22:26:12 +0530

    Maps

---
+++ day_19/README.md
@@ -0,0 +1,5 @@
+# Day 19
+
+# Concepts Practised:
+
+* Maps
---
+++ day_19/maps.dart
@@ -0,0 +1,11 @@
+void main() {
+  Map<String, int> temperatures = {
+    "Bengaluru": 23,
+    "Delhi": 21,
+    "Mumbai": 23
+  };
+
+
+  String city = "Mumbai";
+  print("Temperature in $city is ${temperatures[city]}");
+}
