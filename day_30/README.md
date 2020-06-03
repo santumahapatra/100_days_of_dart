e49b6534f57beb4aed81cb2774fc53827a74be08
Santu Mahapatra <santumahapatra@gmail.com>
2020-06-03 23:02:02 +0430

    More strings

---
+++ day_30/README.md
@@ -0,0 +1,5 @@
+# Day 30
+
+# Concepts Practised:
+
+* String Practice
---
+++ day_30/strings_2.dart
@@ -0,0 +1,5 @@
+void main() {
+  String hello = "hello, world.  ";
+  print(hello.toUpperCase());
+  print(hello.startsWith('l'));
+}
