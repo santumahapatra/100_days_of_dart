aa548b4e360fc5fd9bdc3f87b993f2bff50be8bf
Santu Mahapatra <santumahapatra@gmail.com>
2020-06-04 23:46:50 +0530

    Sleepy

---
+++ day_31/README.md
@@ -0,0 +1,5 @@
+# Day 31
+
+# Concepts Practised:
+
+* Super Sleepy
---
+++ day_31/sleepy.dart
@@ -0,0 +1,6 @@
+void main() {
+  int countToSleep = 10;
+  for(int i = 10; i > 0; i = i - 1){
+    print("Count $i to sleep");
+  }
+}
