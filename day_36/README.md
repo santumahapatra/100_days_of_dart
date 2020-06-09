7fcb76e0f86171f4773f6ef34dba662f4a26d974
Santu Mahapatra <santumahapatra@gmail.com>
2020-06-09 21:37:52 +0530

    StringBuffer class

---
+++ day_36/README.md
@@ -0,0 +1,5 @@
+# Day 35
+
+# Concepts Practised:
+
+* String Buffer
---
+++ day_36/string_buffer.dart
@@ -0,0 +1,6 @@
+void main(){
+  StringBuffer aStory = new StringBuffer();
+  aStory.write("The character woke up in the middle of the night. ");
+  aStory.write("He heard a sound in the dark.");
+  print(aStory);
+}
