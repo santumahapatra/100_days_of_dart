31194b97946ac43bde3c3c7ac653d13cd6785e35
Santu Mahapatra <santumahapatra@gmail.com>
2020-06-15 11:01:09 +0530

    Finishing the word count problem

--- day_32/word-count/lib/word_count.dart
+++ day_32/word-count/lib/word_count.dart
@@ -1,22 +1,10 @@
 class WordCount {
   Map<String, int> countWords(String input){
     Map<String, int> wordCount = {};
-    RegExp expression = new RegExp(r"(\w+)");
+    RegExp expression = new RegExp(r"(\w+('\w+)?)");
     Iterable<RegExpMatch> matches = expression.allMatches(input);
-    // List<String> matches = input.split(new RegExp(r"(\w+)"));
-    // ('\s|,|[\n]')
-    // matches.forEach((String word){
-    //   if (wordCount.containsKey(word)){
-    //     wordCount[word] += 1;
-    //   }
-    //   else {
-    //     wordCount[word] = 1;
-    //   }
-    // });
-
     for (RegExpMatch m in matches){
-      String match = m.group(0);
-      print(match);
+      String match = m.group(0).toLowerCase();
       if (wordCount.containsKey(match)){
         wordCount[match] += 1;
       }
@@ -24,7 +12,6 @@ class WordCount {
         wordCount[match] = 1;
       }
     }
-
     return wordCount;
   }
 }
