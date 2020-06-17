c63e869b46c9d374472fdadbf16b6b9fc2b5f1d9
Santu Mahapatra <santumahapatra@gmail.com>
2020-06-17 19:58:52 +0530

    Bob exercism refactored: Without Regex

--- day_039/bob/lib/bob.dart
+++ day_039/bob/lib/bob.dart
@@ -1,27 +1,46 @@
 class Bob {
-  String response(input){
-    String bobism = "Whatever.";
-    RegExp anySmallCase = new RegExp(r"[a-z]+");
-    RegExp questionRegex = new RegExp(r"[?]$");
-    RegExp anyLargeCase = new RegExp(r"[A-Z]+");
-    RegExp nonWhiteSpace = new RegExp(r"\S");
+  String response(String greeting){
+    // String bobism = "Whatever.";
+    // RegExp anySmallCase = new RegExp(r"[a-z]+");
+    // RegExp questionRegex = new RegExp(r"[?]$");
+    // RegExp anyLargeCase = new RegExp(r"[A-Z]+");
+    // RegExp nonWhiteSpace = new RegExp(r"\S");
 
-    bool yelling = ((anySmallCase.allMatches(input)).length == 0 && (anyLargeCase.allMatches(input)).length > 0);
-    bool question = (questionRegex.allMatches(input.trim())).length > 0;
-    bool allSpaces = (nonWhiteSpace.allMatches(input).length == 0);
-    if (yelling && question){
-      bobism = 'Calm down, I know what I\'m doing!';
+    // bool yelling = ((anySmallCase.allMatches(input)).length == 0 && (anyLargeCase.allMatches(input)).length > 0);
+    // bool question = (questionRegex.allMatches(input.trim())).length > 0;
+    // bool allSpaces = (nonWhiteSpace.allMatches(input).length == 0);
+    // if (yelling && question){
+    //   bobism = 'Calm down, I know what I\'m doing!';
+    // }
+    // else if (yelling){
+    //   bobism = "Whoa, chill out!";
+    // }
+    // else if (question){
+    //   bobism = "Sure.";
+    // }
+    // else if (allSpaces){
+    //   bobism = "Fine. Be that way!";
+    // }
+    // return bobism;
+    greeting = greeting.trim();
+
+    bool spaces = greeting == "";
+    if(spaces){
+      return 'Fine. Be that way!';
     }
-    else if (yelling){
-      bobism = "Whoa, chill out!";
+    bool question = greeting[greeting.length - 1] == "?";
+    bool upperCase = greeting.toUpperCase() == greeting;
+    bool letterCheck = greeting.toLowerCase() != greeting;
+    if(question && upperCase  && letterCheck ){
+      return 'Calm down, I know what I\'m doing!';
     }
-    else if (question){
-      bobism = "Sure.";
+    if(question){
+      return "Sure.";
     }
-    else if (allSpaces){
-      bobism = "Fine. Be that way!";
+    if (upperCase && letterCheck ){
+      return "Whoa, chill out!";
     }
-    return bobism;
+    return "Whatever.";
   }
 
 }
